9f65985f3b18ee37b2e5a1330591bdc242310349
yd-sdccd <ydiaz001@student.sdccd.edu>
2026-07-04 08:46:56 -0700

    Add files via upload

---
+++ w1_first_assembly.asm
@@ -0,0 +1,20 @@
+section .text
+	global _start
+	
+_start:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, len
+	int 0x80
+	
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
+	
+section .data 
+	msg db 'I came,', 0x0A
+	db 'I saw,', 0x0A
+	db 'I conquered.', 0x0A
+		
+	len equ $ - msg
